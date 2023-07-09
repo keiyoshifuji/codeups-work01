d48d356823f946371f461e9aea60a618961e09f7
keiyoshifuji <k2y_c11an_5mi1e@outlook.jp>
2023-07-09 17:01:01 +0900

    headerまで完了

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,9 @@
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
-
+  // * hamburger and drawer
+  $(".js-hamburger , .js-drawer-menu").click(function () {
+    $(".js-hamburger").toggleClass("is-active"); //.is-activeクラスをtoggleする
+    $(".js-drawer-menu").fadeToggle(); //ドロワー（マスク）をfadeIn/Outする
+  });
 });
